593395d26168a7e497d08993a87c6093db76d033
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2025-05-03 21:55:05 -0500

    Revert "fix"
    This reverts commit 37b84c086a3b083840c650ac2565ec10051f57d7.

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -208,8 +208,7 @@ class CarState(CarStateBase):
     ret.gas = cp.vl["POWERTRAIN_DATA"]["PEDAL_GAS"]
     ret.gasPressed = ret.gas > 1e-5
 
-    if self.CP.carFingerprint != CAR.HONDA_CIVIC_NS:
-      ret.steeringTorque = cp.vl["STEER_STATUS"]["STEER_TORQUE_SENSOR"]
+    ret.steeringTorque = cp.vl["STEER_STATUS"]["STEER_TORQUE_SENSOR"]
     ret.steeringTorqueEps = cp.vl["STEER_MOTOR_TORQUE"]["MOTOR_TORQUE"]
     ret.steeringPressed = abs(ret.steeringTorque) > STEER_THRESHOLD.get(self.CP.carFingerprint, 1200)
 
